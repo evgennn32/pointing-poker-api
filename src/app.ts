3ffcca757798374b924115e372db5cf32764d04d
Yauheni Saladukha <evgennn32@gmail.com>
2021-09-14 16:52:48 +0300

    init: add packages and test server

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 4000;
+app.get('/', (req, res) => {
+  res.send('Welcome to pointing poker API');
+});
+app.listen(port, () => {
+  console.log(`server is listening on ${port}`);
+});
