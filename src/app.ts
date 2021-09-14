41a61c22a4b9bd7e7e93e5895f7023a04184fd1b
Yauheni Saladukha <evgennn32@gmail.com>
2021-09-14 20:50:43 +0300

    fix: port

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.get('/', (req, res) => {
   res.send('Welcome to pointing poker API');
 });
