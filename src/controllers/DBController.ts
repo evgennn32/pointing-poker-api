a5e3760ff9baa1dfbb1e7465ae97aa9848f67280
Yauheni Saladukha <evgennn32@gmail.com>
2021-09-15 16:42:23 +0300

    feat: add game create methods, GameController, DBController

---
+++ src/controllers/DBController.ts
@@ -0,0 +1,12 @@
+import { GameRoomEntity } from "../models/GameRoomEntity";
+
+export default {
+  initDB: () => {
+    const DB: { games: { [key: string]: GameRoomEntity } } = {games: {}};
+    global.DB = DB;
+  },
+  addGame: (game: GameRoomEntity) => {
+    global.DB.games[game.roomID] = game;
+    return game.roomID
+  }
+}
---
+++ src/controllers/GameController.ts
@@ -0,0 +1,31 @@
+import { User } from "../models/User";
+import { InitialCards } from "../models/InitialCards";
+import { createId } from "../shared/helpers";
+import DBController from "./DBController";
+
+export const createGame = (user: User ) => {
+  user.id = createId();
+  const gameId = createId();
+  const newGame = {
+    roomName: `Spring ${Object.getOwnPropertyNames(global.DB.games).length + 1}`,
+    roomID: gameId,
+    scramMuster: user,
+    gameSettings: {
+      scrumMasterAsPlayer: true,
+      changingCardInRoundEnd: false,
+      isTimerNeeded: false,
+      scoreType: 'Story Point',
+      scoreTypeShort: 'SP',
+      roundTime: 120,
+      timeOut: true,
+      gameInProgress: false,
+    },
+    users: [user],
+    issues: [],
+    cards: InitialCards
+  };
+
+  DBController.addGame(newGame);
+
+  return newGame;
+}
