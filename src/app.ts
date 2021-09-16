0da0bd0c9c262c66720ce087557f8b767eb3002f
Yauheni Saladukha <evgennn32@gmail.com>
2021-09-16 11:00:08 +0300

    feat: add multigame rooms

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import { User } from "./models/User";
-import  GameController  from "./controllers/GameController";
+import GameController from "./controllers/GameController";
 import DBController from "./controllers/DBController";
+
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http,{
+const io = require('socket.io')(http, {
   cors: {
     origin: '*',
   }
@@ -16,9 +17,24 @@ app.get('/', (req, res) => {
 });
 
 io.on("connection", socket => {
-  socket.on("create:game", ( user: User ) => {
-    const newGame  = GameController.createGame(user);
+  socket.on("create:game", (user: User) => {
+    const newGame = GameController.createGame(user);
+    socket.join(newGame.roomID);
     socket.emit("game:created", newGame);
+    console.log(socket.rooms);
+  });
+  socket.on('game:join', (user: User, roomId: string, cb: ({}) => void) => {
+    console.log('try to join');
+    console.log('game isset', DBController.gameIsset(roomId))
+    console.log(global.DB.games)
+    if (DBController.gameIsset(roomId)) {
+      socket.join(roomId);
+      // TODO create User
+      const newUser = {test: "test"}
+      socket.emit("game:join", newUser);
+    } else {
+      cb({error:'No such game or id is incorrect'});
+    }
   });
   console.log(socket.id)
 });
